a1abf54bdd10992c17908bec6a8a3a1f0b7ecf10
silly0302 <154342017+silly0302@users.noreply.github.com>
2024-09-14 14:19:31 +0900

    [level 2] Title: 구명보트, Time: 9.16 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,16 +1,13 @@
 def solution(people, limit):
-    people.sort()  # 무게순으로 정렬
-    i, j = 0, len(people) - 1  # 투 포인터 사용
+    people.sort()  
+    i, j = 0, len(people) - 1  
     answer = 0
 
     while i <= j:
-        if people[i] + people[j] <= limit:  # 가장 가벼운 사람과 무거운 사람 태움
-            i += 1  # 가벼운 사람 태웠으니 다음 사람으로 이동
-        # 무거운 사람은 항상 태워야 하므로 j는 감소
+        if people[i] + people[j] <= limit:  
+            i += 1  
+       
         j -= 1
-        answer += 1  # 보트 하나 사용
+        answer += 1  
 
     return answer
-
-            
-   
