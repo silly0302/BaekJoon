b8328986e488eb389b5f1750cf1b6d8ac916bffa
김경재 <154342017+silly0302@users.noreply.github.com>
2024-09-24 14:51:05 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,16 @@
+def solution(num):
+    answer = 0
+    while num != 1:
+        if answer < 500:
+            if num % 2 == 0:
+                num = num // 2
+                answer += 1
+            elif num % 2 == 1:
+                num = 3 * num + 1
+                answer += 1
+        else:
+            return -1
+            
+
+        
+    return answer
